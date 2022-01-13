f9572b5a227ec9a6cbc2786c25e0a278b297f095
snowmerak <snowmerak@outlook.com>
2022-01-13 22:37:28 +0900

    Delete globalmiddleware.go

--- globalmiddleware.go
+++
@@ -1,9 +0,0 @@
-package lux
-
-import "github.com/valyala/fasthttp"
-
-var globalMiddleware = []func(ctx *fasthttp.RequestCtx) *fasthttp.RequestCtx{}
-
-func PushGlobalMiddleware(m func(ctx *fasthttp.RequestCtx) *fasthttp.RequestCtx) {
-	globalMiddleware = append(globalMiddleware, m)
-}
