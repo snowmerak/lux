3d672fea920e88142ed74fb41d0e8b88f65535bc
snowmerak <snowmerak@outlook.com>
2022-01-13 22:00:02 +0900

    Update router.go

--- router.go
+++ router.go
@@ -40,46 +40,80 @@ func (r *RouterGroup) UseResponse(m ...middleware.Middleware) *RouterGroup {
 	return r
 }
 
-func (r *RouterGroup) Handle(method string, path string, handler Handler) {
+type Router struct {
+	requestMiddlewares  []middleware.Middleware
+	responseMiddlewares []middleware.Middleware
+}
+
+func (r *Router) UseRequest(m ...middleware.Middleware) *Router {
+	r.requestMiddlewares = append(r.requestMiddlewares, m...)
+	return r
+}
+
+func (r *Router) UseResponse(m ...middleware.Middleware) *Router {
+	r.responseMiddlewares = append(r.responseMiddlewares, m...)
+	return r
+}
+
+func (r *RouterGroup) Handle(method string, path string, handler Handler) *Router {
+	router := &Router{}
 	r.group.Handle(method, path, func(ctx *fasthttp.RequestCtx) {
-		for _, m := range r.requestMiddlewares {
-			ctx = m(ctx)
-		}
 		luxCtx := &LuxContext{ctx: ctx}
+		for _, m := range r.requestMiddlewares {
+			if !luxCtx.Ok() {
+				return
+			}
+			luxCtx.ctx = m(luxCtx.ctx)
+		}
+		for _, m := range router.requestMiddlewares {
+			if !luxCtx.Ok() {
+				return
+			}
+			luxCtx.ctx = m(luxCtx.ctx)
+		}
 		if luxCtx.Ok() {
 			handler(luxCtx)
-			luxCtx.Ok()
-			for _, m := range r.responseMiddleware {
-				ctx = m(luxCtx.ctx)
+		}
+		for _, m := range router.responseMiddlewares {
+			if !luxCtx.Ok() {
+				return
 			}
+			luxCtx.ctx = m(luxCtx.ctx)
+		}
+		for _, m := range r.responseMiddleware {
+			if !luxCtx.Ok() {
+				return
+			}
+			ctx = m(luxCtx.ctx)
 		}
 	})
+	return router
 }
 
-func (r *RouterGroup) Get(path string, handler Handler) {
-	r.Handle(GET, path, handler)
+func (r *RouterGroup) Get(path string, handler Handler) *Router {
+	return r.Handle(GET, path, handler)
 }
 
-func (r *RouterGroup) Post(path string, handler Handler) {
-	r.Handle(POST, path, handler)
+func (r *RouterGroup) Post(path string, handler Handler) *Router {
+	return r.Handle(POST, path, handler)
 }
 
-func (r *RouterGroup) Head(path string, handler Handler) {
-	r.Handle(HEAD, path, handler)
+func (r *RouterGroup) Head(path string, handler Handler) *Router {
+	return r.Handle(HEAD, path, handler)
 }
 
-func (r *RouterGroup) Delete(path string, handler Handler) {
-	r.Handle(DELETE, path, handler)
+func (r *RouterGroup) Delete(path string, handler Handler) *Router {
+	return r.Handle(DELETE, path, handler)
 }
 
-func (r *RouterGroup) Put(path string, handler Handler) {
-	r.Handle(PUT, path, handler)
+func (r *RouterGroup) Put(path string, handler Handler) *Router {
+	return r.Handle(PUT, path, handler)
 }
 
-func (r *RouterGroup) Patch(path string, handler Handler) {
-	r.Handle(PATCH, path, handler)
+func (r *RouterGroup) Patch(path string, handler Handler) *Router {
+	return r.Handle(PATCH, path, handler)
 }
 
-func (r *RouterGroup) Options(path string, handler Handler) {
-	r.Handle(OPTIONS, path, handler)
+func (r *RouterGroup) Options(path string, handler Handler) *Router {
+	return r.Handle(OPTIONS, path, handler)
 }
