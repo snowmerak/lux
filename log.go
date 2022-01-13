962285cafe0195d0338f8e8d838aeb4a09b8052a
snowmerak <snowmerak@outlook.com>
2022-01-13 19:44:59 +0900

    Update log.go

--- log.go
+++ log.go
@@ -1,12 +1,24 @@
 package lux
 
 import (
+	"context"
+
 	"github.com/snowmerak/logstream/log"
+	"github.com/snowmerak/logstream/log/loglevel"
 	"github.com/snowmerak/logstream/log/writable"
+	"github.com/snowmerak/logstream/log/writable/stdout"
 	"github.com/snowmerak/lux/logger"
+	"github.com/snowmerak/lux/middleware"
 )
 
+const SYSTEM = "SYSTEM"
+
 type Logger struct{}
+
+func init() {
+	logger.Observe(SYSTEM, stdout.New(context.Background(), loglevel.All, nil))
+	logger.Observe(middleware.MIDDLEWARE, stdout.New(context.Background(), loglevel.All, nil))
+}
 
 func (l Logger) Write(topic string, log log.Log) error {
 	return logger.Write(topic, log)
