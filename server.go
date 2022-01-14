4fcd9afdb541513e5e33a3e34b944cbc1efeeabc
snowmerak <snowmerak@outlook.com>
2022-01-14 22:34:40 +0900

    Update server.go

--- server.go
+++ server.go
@@ -1,6 +1,7 @@
 package lux
 
 import (
+	"fmt"
 	"os"
 	"time"
 
@@ -46,23 +47,32 @@ func (l *Lux) SetMaxRequestsPerConn(limit int) {
 	l.server.MaxRequestsPerConn = limit
 }
 
+func printInfo(addr string) {
+	fmt.Print(banner)
+	fmt.Printf("Lux is running at %s\n", addr)
+}
+
 func (l *Lux) ListenAndServe(addr string) error {
 	l.server.Handler = l.router.Handler
+	printInfo(addr)
 	return l.server.ListenAndServe(addr)
 }
 
 func (l *Lux) ListenAndServeTLS(addr, certFile, keyFile string) error {
 	l.server.Handler = l.router.Handler
+	printInfo(addr)
 	return l.server.ListenAndServeTLS(addr, certFile, keyFile)
 }
 
 func (l *Lux) ListenAndServeTLSEmbed(addr string, certData, keyData []byte) error {
 	l.server.Handler = l.router.Handler
+	printInfo(addr)
 	return l.server.ListenAndServeTLSEmbed(addr, certData, keyData)
 }
 
 func (l *Lux) ListenAndServeUNIX(addr string, mode os.FileMode) error {
 	l.server.Handler = l.router.Handler
+	printInfo(addr)
 	return l.server.ListenAndServeUNIX(addr, mode)
 }
 
@@ -72,5 +82,6 @@ func (l *Lux) ListenAndServeAutoTLS(addr string) error {
 		return err
 	}
 	l.server.Handler = l.router.Handler
+	printInfo(addr)
 	return l.server.Serve(ln)
 }
