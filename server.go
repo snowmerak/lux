fe6bb1092c0be179e59e60e498505b886a062479
snowmerak <snowmerak@outlook.com>
2022-01-18 02:43:06 +0900

    add comments

--- server.go
+++ server.go
@@ -13,6 +13,12 @@ import (
 	"github.com/valyala/fasthttp"
 )
 
+/*
+Lux ...
+server is a fasthttp.Server
+router is a router.Router
+and has some global middlewares
+*/
 type Lux struct {
 	server              *fasthttp.Server
 	router              *router.Router
@@ -20,6 +26,10 @@ type Lux struct {
 	responseMiddlewares []middleware.Middleware
 }
 
+/*
+NewServer ...
+Create NewServer with default settings.
+*/
 func NewServer() *Lux {
 	return &Lux{
 		server: &fasthttp.Server{
@@ -29,6 +39,10 @@ func NewServer() *Lux {
 	}
 }
 
+/*
+RouterGroup ...
+returns a RouterGroup of paths.
+*/
 func (l *Lux) RouterGroup(path ...string) *RouterGroup {
 	group := l.router.Group("/" + strings.Join(path, "/"))
 	return &RouterGroup{
@@ -38,28 +52,49 @@ func (l *Lux) RouterGroup(path ...string) *RouterGroup {
 	}
 }
 
+//SetRequestBodySize ...
 //SetRequestBodySize sets the maximum request body size.
 //unit: bytes
 func (l *Lux) SetRequestBodySize(size int) {
 	l.server.MaxRequestBodySize = size
 }
 
+/*
+SetReadTimeout ...
+SetReadTimeout sets the maximum duration for reading the entire
+*/
 func (l *Lux) SetReadTimeout(timeout time.Duration) {
 	l.server.ReadTimeout = timeout
 }
 
+/*
+SetWriteTimeout ...
+SetWriteTimeout sets the maximum duration before timing out
+*/
 func (l *Lux) SetWriteTimeout(timeout time.Duration) {
 	l.server.WriteTimeout = timeout
 }
 
+/*
+SetConnPerIPLimit ...
+SetConnPerIPLimit sets the maximum number of concurrent connections per IP.
+*/
 func (l *Lux) SetConnPerIPLimit(limit int) {
 	l.server.MaxConnsPerIP = limit
 }
 
+/*
+SetMaxRequestPerConn ...
+SetMaxRequestPerConn sets the maximum number of requests per connection.
+*/
 func (l *Lux) SetMaxRequestsPerConn(limit int) {
 	l.server.MaxRequestsPerConn = limit
 }
 
+/*
+Use ...
+Use adds a middleware to the server.
+*/
 func (l *Lux) Use(middlewareset ...middleware.MiddlewareSet) {
 	for _, m := range middlewareset {
 		req, res := m.Request, m.Response
@@ -99,30 +134,50 @@ func (l *Lux) wrapHandler() {
 	l.server.Handler = handler
 }
 
+/*
+ListenAndServe ...
+ListenAndServe starts the server over http.
+*/
 func (l *Lux) ListenAndServe(addr string) error {
 	l.wrapHandler()
 	printInfo(addr)
 	return l.server.ListenAndServe(addr)
 }
 
+/*
+ListenAndServeTLS ...
+ListenAndServeTLS starts the server over https with cert file and key file.
+*/
 func (l *Lux) ListenAndServeTLS(addr, certFile, keyFile string) error {
 	l.wrapHandler()
 	printInfo(addr)
 	return l.server.ListenAndServeTLS(addr, certFile, keyFile)
 }
 
+/*
+ListenAndServeAutoTLSEmbed ...
+ListenAndServeAutoTLSEmbed starts the server over https with embedded cert data and key data.
+*/
 func (l *Lux) ListenAndServeTLSEmbed(addr string, certData, keyData []byte) error {
 	l.wrapHandler()
 	printInfo(addr)
 	return l.server.ListenAndServeTLSEmbed(addr, certData, keyData)
 }
 
+/*
+ListenAndServeUNIX ...
+ListenAndServeUNIX starts the server over unix socket.
+*/
 func (l *Lux) ListenAndServeUNIX(addr string, mode os.FileMode) error {
 	l.wrapHandler()
 	printInfo(addr)
 	return l.server.ListenAndServeUNIX(addr, mode)
 }
 
+/*
+ListenAndServeAutoTLS ...
+ListenAndServeAutoTLS starts the server over https with cert file and key file automatic generated.
+*/
 func (l *Lux) ListenAndServeAutoTLS(addr string) error {
 	ln, err := certmagic.Listen([]string{addr})
 	if err != nil {
