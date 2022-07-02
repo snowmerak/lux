036b614618778cfbe036747d8382a3f33077663b
snowmerak <snowmerak@outlook.com>
2022-07-02 11:06:44 +0900

    update nodePool and GC

--- session/session.go
+++ session/session.go
@@ -2,6 +2,8 @@ package session
 
 import (
 	"errors"
+	"sync"
+	"sync/atomic"
 	"time"
 
 	"github.com/Workiva/go-datastructures/trie/ctrie"
@@ -13,17 +15,74 @@ type node struct {
 	value        interface{}
 }
 
+// nodePool is a pool of node
+var nodePool = sync.Pool{
+	New: func() interface{} {
+		return &node{}
+	},
+}
+
 // Local is a session of app scope
 type Local struct {
-	data ctrie.Ctrie
-	ttl  int64
+	data    ctrie.Ctrie
+	ttl     int64
+	count   int64
+	prevGC  int64
+	counter chan struct{}
 }
 
 // NewLocal create new Local instance
 func NewLocal() *Local {
 	return &Local{
-		data: *ctrie.New(nil),
+		data:    *ctrie.New(nil),
+		counter: make(chan struct{}, 1),
 	}
+}
+
+// IncreaseCount is increasing count of local session
+func (l *Local) IncreaseCount() {
+	atomic.AddInt64(&l.count, 1)
+	v := atomic.LoadInt64(&l.count)
+	p := atomic.LoadInt64(&l.prevGC)
+	if p*3/2 <= v {
+		l.counter <- struct{}{}
+	}
+}
+
+// DecreaseCount is decreasing count of local session
+func (l *Local) DecreaseCount() {
+	atomic.AddInt64(&l.count, -1)
+}
+
+// StartGC is starting GC process
+func StartGC(localSession *Local) {
+	gc := func() {
+		cancel := make(chan struct{}, 1)
+		iter := localSession.data.Iterator(cancel)
+		for p := range iter {
+			n, ok := p.Value.(*node)
+			if !ok {
+				continue
+			}
+			if n.latestAccess+localSession.ttl < time.Now().Unix() {
+				localSession.DecreaseCount()
+				localSession.data.Remove(p.Key)
+				nodePool.Put(n)
+			}
+		}
+		atomic.StoreInt64(&localSession.prevGC, atomic.LoadInt64(&localSession.count))
+	}
+	go func() {
+		interval := time.NewTicker(time.Duration(localSession.ttl) * 125 / 100)
+		for {
+			select {
+			case <-localSession.counter:
+				gc()
+			case <-interval.C:
+				gc()
+			}
+		}
+	}()
 }
 
 // SetTTL set Local's ttl to given duration
@@ -68,13 +127,18 @@ func GetLocal[T any](localSession *Local, key []byte) (*T, error) {
 		return nil, errNotMatchType
 	}
 	if n.latestAccess+localSession.ttl >= time.Now().Unix() {
+		localSession.DecreaseCount()
 		localSession.data.Remove(key)
+		nodePool.Put(n)
 		return nil, errTimeout
 	}
 	t, ok := n.value.(*T)
 	if !ok {
 		return nil, errNotMatchType
 	}
+	n.latestAccess = time.Now().Unix()
+	localSession.IncreaseCount()
+	localSession.data.Insert(key, n)
 	return t, nil
 }
 
@@ -92,15 +156,21 @@ func SetLocal[T any](localSession *Local, key []byte, value T) error {
 	if d, ok := localSession.data.Lookup(key); ok {
 		n, ok := d.(*node)
 		if !ok {
+			localSession.DecreaseCount()
 			localSession.data.Remove(key)
-			return errNotMatchType
 		}
 		if n.latestAccess+localSession.ttl < time.Now().Unix() {
 			return errAlreadyExists
 		}
+		localSession.DecreaseCount()
 		localSession.data.Remove(key)
+		nodePool.Put(n)
 	}
-	localSession.data.Insert(key, &value)
+	n := nodePool.Get().(*node)
+	n.latestAccess = time.Now().Unix()
+	n.value = &value
+	localSession.IncreaseCount()
+	localSession.data.Insert(key, n)
 	return nil
 }
 
@@ -112,9 +182,15 @@ func RemoveLocal[T any](localSession *Local, key []byte) (*T, error) {
 	if !ok {
 		return nil, errNotExists
 	}
-	t, ok := data.(*T)
+	n, ok := data.(*node)
 	if !ok {
 		return nil, errNotMatchType
 	}
+	t, ok := n.value.(*T)
+	if !ok {
+		return nil, errNotMatchType
+	}
+	localSession.DecreaseCount()
+	nodePool.Put(n)
 	return t, nil
 }
